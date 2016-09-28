33ef816c9dab7bfea92b33a5f27f9c2fdddd7463
sigrid viaene <sigridviaene@gmail.com>
2016-09-28 15:20:57 +0200

    Seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+
+3.times do
+  u = User.new(email: Faker::Internet.email, first_name: Faker::Name.first_name, last_name:Faker::Name.last_name, facebook_picture_url: Faker::Avatar.image, city_name:Faker::Address.city)
+  u.save
+end
