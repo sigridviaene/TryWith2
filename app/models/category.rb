c0c437b43b9f3f081f720132d51fb234185735f3
sigrid viaene <sigridviaene@gmail.com>
2016-10-26 17:29:26 +0200

    add category model & migration

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
+<<<<<<< HEAD
   has_many :experiences
+=======
+  belongs_to :experiences
+>>>>>>> 7301fca250274e629653d6351eea84d24c6829e4
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,4 +1,8 @@
 class Experience < ApplicationRecord
   belongs_to :user
+<<<<<<< HEAD
   belongs_to :category
+=======
+  has_many :categories
+>>>>>>> 7301fca250274e629653d6351eea84d24c6829e4
 end
